7620aa1feb1d661ec9990414209f21990e359fe2
Aric Camarata <alisalaah@gmail.com>
2014-12-24 09:52:25 +0700

    minor update to description and version number (again)

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
 	name: 'alisalaah:currencies',
 	summary: 'Meteor package to use live currencies and exchange rates in your app',
-	version: '1.0.2',
+	version: '2.0.0',
 	git: 'https://github.com/alisalaah/meteor-currencies.git'
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.0.1');
+	api.versionsFrom('METEOR@0.9.2');
 	api.use(['http'], 'server');
 	api.use(['ui'], 'client');
 	
