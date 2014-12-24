adc36d184494bff13795213909ca8c088fd7ba34
Aric Camarata <alisalaah@gmail.com>
2014-12-24 18:42:07 +0700

    updated version number for updated currency symbols from pull

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'alisalaah:currencies',
 	summary: 'Meteor package to use live currencies and exchange rates in your app',
-	version: '2.0.0',
+	version: '2.1.0',
 	git: 'https://github.com/alisalaah/meteor-currencies.git'
 });
 
